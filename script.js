26a3ec2823286e96e7e9192c43d0fa83d7dfdf7f
Walid Neji <walid.neji@ratp.fr>
2025-03-07 15:32:08 +0100

    init site

---
+++ script.js
@@ -0,0 +1,4 @@
+// Vous pouvez ajouter des fonctionnalités interactives ici si nécessaire
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Site BH2N Consulting chargé');
+});
